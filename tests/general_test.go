ac9a670c4c3c1c8e170cfb2f0feebd9b8e5ff4f3
Dom Clayton <dominic@booandjoel.co.uk>
2024-10-29 14:40:01 +0000

    Updated test and commented out report

--- tests/general_test.go
+++ tests/general_test.go
@@ -1,8 +1,11 @@
 package test
 
 import (
+	"crypto/tls"
 	"testing"
+	"time"
 
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
@@ -17,11 +20,11 @@ func TestSmoke(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
 
-	// smokeTestText := "Enter your index:"
-	// smokeUrl := terraform.Output(t, terraformOptions, "ingress")
-	// tlsConfig := tls.Config{}
-	// maxRetries := 30
-	// timeBetweenRetries := 5 * time.Second
+	smokeTestText := "Enter your index:"
+	smokeUrl := terraform.Output(t, terraformOptions, "ingress")
+	tlsConfig := tls.Config{}
+	maxRetries := 30
+	timeBetweenRetries := 5 * time.Second
 
-	// http_helper.HttpGetWithRetry(t, smokeUrl, &tlsConfig, 200, smokeTestText, maxRetries, timeBetweenRetries)
+	http_helper.HttpGetWithRetry(t, smokeUrl, &tlsConfig, 200, smokeTestText, maxRetries, timeBetweenRetries)
 }
